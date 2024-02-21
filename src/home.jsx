dd1c8568a08afad12865f4aa1032d5f9a9dd0c96
AnnasAhmed1 <annasahmed1609@gmail.com>
2024-02-21 21:03:45 +0500

    chking

---
+++ src/home.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  PinterestIcon,
+  PinterestShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
+const Home = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Home title</title>
+        <meta
+          property="og:image"
+          content="https://media.gettyimages.com/id/184944186/photo/quaid-e-azam.jpg?s=612x612&w=0&k=20&c=7mRHDKfBWbpmiTto_w_oMm4EeboU9tEDO_JXke01P5I="
+        />
+        <meta property="og:title" content="Home Image title" />
+        <meta property="og:description" content="Home Image description" />
+      </Helmet>
+      Home
+      <FacebookShareButton
+        url={"https://social-sharing-nextjs-check.vercel.app/newpage"}
+      >
+        <FacebookIcon size={32} round={true} style={{ color: "black" }} />
+      </FacebookShareButton>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/login.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import {
+  EmailIcon,
+  EmailShareButton,
+  FacebookIcon,
+  FacebookShareButton,
+  PinterestIcon,
+  PinterestShareButton,
+  TelegramIcon,
+  TelegramShareButton,
+  TwitterIcon,
+  TwitterShareButton,
+  WhatsappIcon,
+  WhatsappShareButton,
+} from "react-share";
+const Login = () => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>login title</title>
+        <meta
+          property="og:image"
+          content="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        />
+        <meta property="og:title" content="login Image title" />
+        <meta property="og:description" content="Login Image description" />
+      </Helmet>
+      Login
+      <FacebookShareButton
+        url={"https://social-sharing-nextjs-check.vercel.app/newpage"}
+      >
+        <FacebookIcon size={32} round={true} style={{ color: "black" }} />
+      </FacebookShareButton>
+    </div>
+  );
+};
+
+export default Login;
